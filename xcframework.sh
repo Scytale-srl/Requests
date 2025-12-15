0aba1a9a5c7b3d7403be13b75d827be861fff4b3
Francesco Bianco <francesco.bianco@me.com>
2025-12-15 12:18:57 +0100

    Fixes xcframework script to add dSYM

--- xcframework.sh
+++ xcframework.sh
@@ -5,20 +5,23 @@ SCHEME_NAME="Requests"
 FRAMEWORK_NAME="Requests"
 PLATFORM="iphoneos"
 OUTPUT_PATH="Output"
+PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 
 # Per dispositivi:
-xcodebuild archive -scheme $SCHEME_NAME -destination 'generic/platform='$PLATFORM'' -archivePath 'build/device.xcarchive' SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+xcodebuild archive -scheme $SCHEME_NAME -destination 'generic/platform='$PLATFORM'' -archivePath "$PROJECT_DIR/build/device.xcarchive" SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
 
 # Per simulatori:
-xcodebuild archive -scheme $SCHEME_NAME -destination 'generic/platform=iOS Simulator' -archivePath 'build/simulator.xcarchive' SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+xcodebuild archive -scheme $SCHEME_NAME -destination 'generic/platform=iOS Simulator' -archivePath "$PROJECT_DIR/build/simulator.xcarchive" SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
 
 
-# Crea l'xcframeworki
+# Crea l'xcframeworki con dSYM
 xcodebuild -create-xcframework \
--framework 'build/device.xcarchive/Products/Library/Frameworks/'$FRAMEWORK_NAME'.framework' \
--framework 'build/simulator.xcarchive/Products/Library/Frameworks/'$FRAMEWORK_NAME'.framework' \
--output $OUTPUT_PATH'/'$FRAMEWORK_NAME'.xcframework'
+-framework "$PROJECT_DIR/build/device.xcarchive/Products/Library/Frameworks/$FRAMEWORK_NAME.framework" \
+-debug-symbols "$PROJECT_DIR/build/device.xcarchive/dSYMs/$FRAMEWORK_NAME.framework.dSYM" \
+-framework "$PROJECT_DIR/build/simulator.xcarchive/Products/Library/Frameworks/$FRAMEWORK_NAME.framework" \
+-debug-symbols "$PROJECT_DIR/build/simulator.xcarchive/dSYMs/$FRAMEWORK_NAME.framework.dSYM" \
+-output "$PROJECT_DIR/$OUTPUT_PATH/$FRAMEWORK_NAME.xcframework"
 
 
 echo "XCFramework creato con successo in $OUTPUT_PATH"
